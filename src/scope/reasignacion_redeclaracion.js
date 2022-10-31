a8329e9dbdee4ea9905579101d452fd6e7c57b0b
Carlos C <contact@carloscolin.me>
2022-10-31 16:41:14 -0600

    Reasignacion y redeclaracion en var, let y const

---
+++ src/scope/reasignacion_redeclaracion.js
@@ -0,0 +1,26 @@
+var firstName; // undefined
+
+firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Doe'; // declarar y asignar
+console.log(lastName);
+lastName = 'Jane'; // reasignar
+console.log(lastName);
+
+var secondName = 'Janeth'; // declarar y asignar
+console.log(secondName);
+var secondName = 'Anna'; // redeclarar
+console.log(secondName);
+
+
+// Let
+let fruit = 'Apple';
+fruit = 'Banana';
+// let fruit = 'Kiwi'; // No se puede redeclarar y 'let'
+console.log(fruit)
+
+
+// Const
+const vegetable = 'Carrot';
+// vegetable = 'Potato'; // No se puede reasignar un 'const'
