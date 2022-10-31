9f28639aa431997d5e5f35aac72e9ed9b689d246
Carlos C <contact@carloscolin.me>
2022-10-31 16:26:47 -0600

    Function scope

---
+++ src/scope/function.js
@@ -0,0 +1,13 @@
+let username = 'carlos';
+
+function sayHi() {
+    let username = 'Ana';
+    console.log(username);
+
+    if (username === 'Ana') {
+        console.log(`Hello, ${username}`);
+    }
+}
+
+sayHi();
+console.log(username)
