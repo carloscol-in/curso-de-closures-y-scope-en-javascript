70f5ac37cb847f811b96af78167ea1ae72e90f7f
Carlos C <contact@carloscolin.me>
2022-11-04 17:47:36 -0600

    Practicing hoisting

---
+++ src/hoisting/index.js
@@ -0,0 +1,9 @@
+// console.log(nameOfDog)
+// var nameOfDog = 'Lucky'
+// console.log(nameOfDog)
+
+console.log(nameOfDog())
+function nameOfDog() {
+    return `El nombre del perrito es: ${dogName}`
+}
+var dogName = 'Lucky'
